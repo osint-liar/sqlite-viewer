482863031d3b29be3a2ca18da6ecb7e41f47b3cd
yooper <dcardin2007@gmail.com>
2023-08-06 13:58:17 -0400

    send as a bearer token

--- js/main.js
+++ js/main.js
@@ -99,7 +99,9 @@ $(".no-propagate").on("click", function (el) { el.stopPropagation(); });
 const searchParams = new URLSearchParams(window.location.search)
 if (searchParams.has('Uuid') && searchParams.has('AuthToken')){
     setIsLoading(true);
-    fetch(`http://127.0.0.1:9906/v1/content/raw?Uuid=${searchParams.get('Uuid')}&AuthToken=${searchParams.get('AuthToken')}`)
+    fetch(`http://127.0.0.1:9906/v1/content/raw?Uuid=${searchParams.get('Uuid')}`, {
+        headers: {Authentication: `Bearer ${searchParams.get('AuthToken')`}
+    })
           .then((response) => response.blob())
           .then((blob) => {
               loadDB(blob.arrayBuffer())
